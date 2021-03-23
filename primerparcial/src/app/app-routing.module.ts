dae6032a2affa0799fbd3445cc7d8355ea5ab80a
Ajdouglas2423 <douglas2423@gmail.com>
2021-03-23 18:55:24 -0500

    Update app-routing.module.ts

--- primerparcial/src/app/app-routing.module.ts
+++ primerparcial/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
+import { PersonaConsultaComponent } from './hotel/persona-consulta/persona-consulta.component';
+import { PersonaRegistroComponent } from './hotel/persona-registro/persona-registro.component';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+  path: 'personaConsulta',
+  component: PersonaConsultaComponent
+  },
+  {
+  path: 'personaRegistro',
+  component: PersonaRegistroComponent
+  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
