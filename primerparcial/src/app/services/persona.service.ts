d339194522f961a1757c515929846670f806b44e
Ajdouglas2423 <douglas2423@gmail.com>
2021-03-23 23:45:54 -0500

    Update persona.service.ts

--- primerparcial/src/app/services/persona.service.ts
+++ primerparcial/src/app/services/persona.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Persona } from '../hotel/models/persona';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,17 @@ import { Injectable } from '@angular/core';
 export class PersonaService {
 
   constructor() { }
+  get(): Persona[] {
+    return JSON.parse(localStorage.getItem('datos'));
+  }
+  post(persona: Persona) {
+    
+    let personas: Persona[] = [];
+    if (this.get() != null) {
+      personas = this.get();
+    }
+    personas.push(persona);
+    localStorage.setItem('datos', JSON.stringify(personas));
+  }
+
 }
