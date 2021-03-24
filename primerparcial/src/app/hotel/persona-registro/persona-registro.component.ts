4875a280f35626290114e2d65e56c2556a8032e5
Ajdouglas2423 <douglas2423@gmail.com>
2021-03-23 23:45:26 -0500

    Update persona-registro.component.ts

--- primerparcial/src/app/hotel/persona-registro/persona-registro.component.ts
+++ primerparcial/src/app/hotel/persona-registro/persona-registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PersonaService } from 'src/app/services/persona.service';
+import { Persona } from './../models/persona';
 
 @Component({
   selector: 'app-persona-registro',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persona-registro.component.css']
 })
 export class PersonaRegistroComponent implements OnInit {
+  persona: Persona;
 
-  constructor() { }
+  constructor(private personaService: PersonaService) { }
+  ngOnInit() {
+    this.persona = new Persona();
+  }
 
-  ngOnInit(): void {
+  add() {
+    alert('Persona Agregada' + JSON.stringify(this.persona));
+    this.personaService.post(this.persona);
   }
 
 }
